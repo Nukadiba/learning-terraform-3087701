54a175cc1771fce87e4d4620279cd1eb8d0e8673
Nukadiba <122801813+Nukadiba@users.noreply.github.com>
2024-09-27 20:42:37 +0100

    alb

--- main.tf
+++ main.tf
@@ -82,3 +82,16 @@ module "alb" {
     Environment = "dev"
   }
 }
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id = module.blog_vpc.vpc_id
+  name = "blog"
+  ingress_rules        = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks  = ["0.0.0.0/0"]
+
+  egress_rules         = ["all-all"]
+  egress_cidr_blocks   = ["0.0.0.0/0"]
+}
